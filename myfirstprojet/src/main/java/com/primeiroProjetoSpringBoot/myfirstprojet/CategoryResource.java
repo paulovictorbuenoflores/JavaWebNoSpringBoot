293e6a8c503641af6ce5e26833c516dc3c52c5d1
pv <paulovictorflores28@gmail.com>
2022-08-09 02:07:17 -0300

    mapeamento objeto relacional 1 para * e * para 1, manyToOne and OneToMany

--- myfirstprojet/src/main/java/com/primeiroProjetoSpringBoot/myfirstprojet/CategoryResource.java
+++ myfirstprojet/src/main/java/com/primeiroProjetoSpringBoot/myfirstprojet/CategoryResource.java
@@ -1,6 +1,6 @@
 package com.primeiroProjetoSpringBoot.myfirstprojet;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- myfirstprojet/src/main/java/com/primeiroProjetoSpringBoot/myfirstprojet/Product.java
+++ myfirstprojet/src/main/java/com/primeiroProjetoSpringBoot/myfirstprojet/Product.java
@@ -2,14 +2,28 @@ package com.primeiroProjetoSpringBoot.myfirstprojet;
 
 import java.io.Serializable;
 
+//import org.hibernate.annotations.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Product implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Double price;
 
     // composicao, relacao 1 para muitos
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
 
     public Product() {
